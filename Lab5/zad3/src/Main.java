b5429e1ff3ef140228c8c4379ea82d8811af1790
WILLI3T <maciekciombor@gmail.com>
2023-04-05 20:08:57 +0200

    zad4

--- Lab5/zad3/src/Main.java
+++ Lab5/zad3/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Car[] tab = {new Krowowóz("zielony","Fiat",15),new Porshe("ruszofy","Porche",true), new Jeep("yes","fajna",5)};
+        for (i:tab
+             ) {
 
+
+        }
     }
 }
