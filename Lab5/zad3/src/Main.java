772c1679e7a84c6b8843b3b53774de2cfa93083b
WILLI3T <maciekciombor@gmail.com>
2023-04-05 20:44:17 +0200

    poprawki

--- Lab5/zad3/src/Main.java
+++ Lab5/zad3/src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Car[] tab = {new Krowowóz("zielony","Fiat",15),new Porshe("ruszofy","Porche",true), new Jeep("yes","fajna",5)};
-        for (i:tab
+        for (Car car:tab
              ) {
-
-
+            System.out.println(car.getMarka()+ " " + car.getColor());
         }
     }
 }
